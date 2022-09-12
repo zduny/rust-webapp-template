46c31885b6ab9965da384ad3a1cc4f0afea31815
Daniel Zduniak <dzduniak@gmail.com>
2022-09-12 10:28:10 +0200

    Add clippy and format scripts.

---
+++ clippy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+echo "Linting common..."
+cd common
+cargo clippy
+cd ..
+
+echo "\nLinting server..."
+cd server
+cargo clippy
+cd ..
+
+echo "\nLinting client..."
+cd client
+cargo clippy
+cd ..
