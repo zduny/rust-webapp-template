298362de62fc5359a65a95bf062b871d72f87180
Daniel Zduniak <dzduniak@gmail.com>
2022-09-12 10:27:45 +0200

    Remove wee_alloc.

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,9 +1,5 @@
 use wasm_bindgen::prelude::*;
 
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[cfg(feature = "panic_hook")]
 pub fn set_panic_hook() {
     console_error_panic_hook::set_once();
