f19e76529f31e32b3af33ed18356d703d582ddb5
Daniel Zduniak <dzduniak@gmail.com>
2022-11-11 08:25:20 +0100

    Update worker.

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -2,7 +2,7 @@ use wasm_bindgen::prelude::*;
 use js_utils::set_panic_hook;
 
 #[wasm_bindgen(start)]
-pub async fn main() -> Result<(), JsValue> {
+pub async fn main_worker() -> Result<(), JsValue> {
     set_panic_hook();
 
     Ok(())
